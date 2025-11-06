d8011881dfcacfb68c52a1c3b4a1c7e8aa8857cb
Douglas Dreer <douglasdreer@outlook.com.br>
2025-11-06 09:38:16 -0300

    initialize manager-product-service project with Gradle and Spring Boot setup

---
+++ src/main/kotlin/br/com/barbers_forge/manager_product_service/ManagerProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.barbers_forge.manager_product_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagerProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManagerProductServiceApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/barbers_forge/manager_product_service/ManagerProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.barbers_forge.manager_product_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagerProductServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
